e297dccbf8f6560ba6a8511f71c12b55cbc66dfe
Logan Weeden <loganaweeden@hotmail.co.uk>
2019-09-04 15:12:29 +0100

    Added replay funcion

--- Guess the Number/Guess the Number/Program.cs
+++ Guess the Number/Guess the Number/Program.cs
@@ -15,46 +15,55 @@ namespace Guess_the_Number
             Console.WriteLine("Guess the Number By Logan Weeden");
             Console.WriteLine("");
             Console.WriteLine("Guess A Randomly gnerated number");
-
-            Random rand = new Random();
-            int NoofAttempts = 0;
-            int ranNumber = rand.Next(1,10);
-            Console.WriteLine("the number is " + ranNumber);
-
-            Console.WriteLine("enter a guess between 1 and 9");
-            int Guess = Convert.ToInt32(Console.ReadLine());
-            NoofAttempts = NoofAttempts + 1;
-            Console.WriteLine("Youe Guessed " +   "" + Guess);
-
-            if (ranNumber == Guess)
+            bool game = true;
+            while (game == true)
             {
-                Console.WriteLine("Correct");
-            }
+                Console.WriteLine("generating new number");
+                Random rand = new Random();
+                int NoofAttempts = 0;
+                int ranNumber = rand.Next(1, 10);
+                Console.WriteLine("the number is " + ranNumber);
 
-            
-            
-            while (Guess != ranNumber)
-            {
-                Console.WriteLine("Wrong Try Again");
-
-                if (ranNumber < Guess)
-                {
-                    Console.WriteLine("Guess Lower");
-                }
-                else
-                {
-                    Console.WriteLine("Guess Higher");
-                }
-                //Read a new guess from the player
                 Console.WriteLine("enter a guess between 1 and 9");
-                Guess = Convert.ToInt32(Console.ReadLine());
+                int Guess = Convert.ToInt32(Console.ReadLine());
                 NoofAttempts = NoofAttempts + 1;
+                Console.WriteLine("Youe Guessed " + "" + Guess);
+
+                while (Guess != ranNumber)
+                {
+                    Console.WriteLine("Wrong Try Again");
+
+                    if (ranNumber < Guess)
+                    {
+                        Console.WriteLine("Guess Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Guess Higher");
+                    }
+                    //Read a new guess from the player
+                    Console.WriteLine("enter a guess between 1 and 9");
+                    Guess = Convert.ToInt32(Console.ReadLine());
+                    NoofAttempts = NoofAttempts + 1;
 
 
+                }
+
+                Console.WriteLine("Correct, it took you " + NoofAttempts + " attempts");
+
+                Console.WriteLine("play again? (Y/N)");
+                string replay = Console.ReadLine();
+                if (replay == "y")
+                {
+                    game = true;
+                }
+                if (replay =="n")
+                {
+                    game = false;
+                }
             }
-
-            Console.WriteLine("Correct, it took you " + NoofAttempts + " attempts");
-            Console.ReadKey();
+            
+                    
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
